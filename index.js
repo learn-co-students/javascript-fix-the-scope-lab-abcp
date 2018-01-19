46bcc32833740f28702ba25432c5bdc740651583
River Magnetic <magneticcontinuum@gmail.com>
2018-01-19 04:20:05 +0000

    Done.

--- index.js
+++ index.js
@@ -22,7 +22,6 @@ var funkyFunction = function() {
     return "FUNKY!"
   }
 }
-
+var theFunk = funkyFunction()()
 // We want to set theeFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction()()
