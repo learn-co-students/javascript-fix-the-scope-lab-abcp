2ab08d61e7783be71852834e90c2dafc1d76ca86
River Magnetic <magneticcontinuum@gmail.com>
2018-01-19 03:49:28 +0000

    Done.

--- index.js
+++ index.js
@@ -14,11 +14,7 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  const two = 2
-  return n + two
-
-  // Feel free to move things around!
-  
+  return n + 2
 }
 
 var funkyFunction = function() {
@@ -27,6 +23,6 @@ var funkyFunction = function() {
   }
 }
 
-// We want to set theFunk equal to "FUNKY!" using our funkyFunction.
+// We want to set theeFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+var theFunk = funkyFunction()()
